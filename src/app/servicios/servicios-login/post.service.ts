2880bbf330ef5d3bc061d611679a66c2ca3ba104
cristian <critian94ruiz@gmail.com>
2024-06-08 18:32:43 +0200

    cambio usuMod

--- src/app/servicios/servicios-login/post.service.ts
+++ src/app/servicios/servicios-login/post.service.ts
@@ -4,6 +4,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
+import { HttpHeaders } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
@@ -14,13 +15,21 @@ export class PostService {
   private apiURL = "http://localhost:8000/api/usuarios";
   constructor(private http: HttpClient, private route: ActivatedRoute) { }
 
+  private getAuthHeaders(): HttpHeaders {
+    const token = localStorage.getItem("token");
+    let headers = new HttpHeaders();
+    if (token) {
+      headers = headers.set('Authorization', `Bearer ${token}`);
+    }
+    return headers;
+  }
   guardarUsuario(usuario: CrearUsuario): Observable<any> {
     return this.http.post(this.apiURL, usuario);
   }
 
   modificarUsuario(usuario: CrearUsuario): Observable<any> {
     const usuarioId = Number(this.route.snapshot.params['id'])
-    return this.http.put(`${this.apiURL}/editar/${usuarioId}`, usuario);
+    return this.http.put(`${this.apiURL}/editar/${usuarioId}`, usuario);//{ headers: this.getAuthHeaders()};
   }
 
 }
